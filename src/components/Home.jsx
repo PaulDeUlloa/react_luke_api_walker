a83abcda0393619a17a97d1d92ea6d1caea9b5c7
Paul De Ulloa <pauld143@gmail.com>
2023-09-06 15:13:56 -0700

    Home edits made

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+    const [resource, setResource] = useState({option: "people", id:""});
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setResource({...resource, [e.target.name]: e.target.value});
+    };
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        navigate(`/${resource.option}/${resource.id}/`);
+    };
+
+    return(
+        <div className="container">
+            <h1>The Star Wars API</h1>
+            <form onSubmit={handleSearch}>
+                <label>Search for: </label>
+                <select
+                name="option"
+                value={resource.option}
+                onChange={handleChange}
+                >
+                    <option></option>
+                </select>
+            </form>
+        </div>
+    )
+
+
+}
+
+export default Home;
