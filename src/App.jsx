ae262d55055f346c5fdbfeb0187a6760b394faf8
Paul De Ulloa <pauld143@gmail.com>
2023-09-06 14:22:54 -0700

    New files added and adits made to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-
+import React from 'react';
+import { Routes, Route } from "react-router-dom"
+import Home from './components/Home'
+import Home from './components/People'
+import Error from './components/Error'
 import './App.css';
 
 function App() {
